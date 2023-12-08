42c4cbc52445c1750395615667907209e0b13765
oussox <oussamaoubahim@gmail.com>
2023-12-08 20:02:39 +0100

    fixed view

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import Landing from "./pages/Landing.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <NextUIProvider>
-    <Landing />
-  </NextUIProvider>
-  // {/* </React.StrictMode> */}
+  <>
+    <NextUIProvider>
+      <Landing />
+      <Analytics />
+    </NextUIProvider>
+  </>
 );
